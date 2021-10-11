c22760470d74258a0820dfeea057ab5a59c751ef
Andrey Litvinsky <60661362+litvinsky@users.noreply.github.com>
2021-10-11 12:15:56 +0200

    Create notify.sh

---
+++ telegram-notify/notify.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#My dummy script to check Humanode services
+
+#Default message text
+message="!!! ATTENTION !!! You must renew your authentification token in Humanode"
+
+humanode=$(ps aux | grep humanode)
+ngork=$(ps aux | grep ngork)
+
+#Check if we missed a "consensus" word in log
+alert=$(journalctl -u humanode -S "$(date -d "-10 minutes" +%Y"-"%m"-"%d" "%T)" | grep consensus)
+
+if [ -z "$alert" ]
+then
+  systemctl restart humanode && \
+  
+  #Wait for humanode startup
+  sleep 10  && \
+  
+  #Get auth url from log
+  link=$(journalctl -u humanode -S "$(date -d "-10 seconds" +%Y"-"%m"-"%d" "%T)" | grep -o 'https://[^"]*')
+  telegram-send "$message Please visit $link"
+fi
+
+#Check if humanode service is working
+if [ -z "$humanode" ]
+then
+  telegram-send "!!! Humanode daemon is not running !!!"
+fi
+
+#Check if ngork service is working
+if [ -z "$ngork" ]
+then
+  telegram-send "!!! Ngrok daemon is not running !!!"
+fi

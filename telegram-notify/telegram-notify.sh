5902c0fff72ea868089888253b89447c30717ed5
Andrey Litvinsky <60661362+litvinsky@users.noreply.github.com>
2021-10-11 12:25:41 +0200

    Create telegram-notify.sh

---
+++ telegram-notify/telegram-notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+printf "#!/bin/bash
+
+GROUP_ID="TELEGRAM_GROUP_ID"
+BOT_TOKEN="YOUR_TELEGRAM_TOKEN"
+
+curl -s --data "text=$1" --data "chat_id=$GROUP_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' > /dev/null" > /usr/bin/telegram-send
+
+chmod +x /usr/bin/telegram-send && \
+chown root:root /usr/bin/telegram-send
+telegram-send "Test!"

4a71060f11b3454d0f9d444394964ab6a028d9e1
Andrey Litvinsky <60661362+litvinsky@users.noreply.github.com>
2021-10-11 15:53:34 +0200

    Create telegram-notify.sh

---
+++ telegram-notify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+printf "#!/bin/bash
+GROUP_ID="TELEGRAM_GROUP_ID"
+BOT_TOKEN="YOUR_TELEGRAM_TOKEN"
+curl -s --data "text=$1" --data "chat_id=$GROUP_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' > /dev/null" > /usr/bin/telegram-send
+
+chmod +x /usr/bin/telegram-send && \
+chown root:root /usr/bin/telegram-send
+crontab -l | { cat; echo "*/15 * * * * /$HOME/humanode/notify.sh >/dev/null 2>&1"; } | crontab -
+telegram-send "Test!"

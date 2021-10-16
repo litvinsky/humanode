cc102917c3cbd1bbf69a50aea3f8099643396511
surugh <35753081+surugh@users.noreply.github.com>
2021-10-16 12:25:35 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,52 +2,60 @@
 
 # REPLACE DEMO VALUES WITH YOUR VALUES
 NAME=DEMO_NODE_NAME
-RPC_URL=DEMO_UNIQUE_RPC_URL
 NGROK_TOKEN=DEMO_NGROK_TOKEN
 SECRET_PHRASE=DEMO_SECRET_PHRASE
+USER_HOME="/home/your_user_name"
 
-echo "Setting up Humanode service..."
-apt install curl jq -y
+echo "Check for humanode & ngrok services..."
+if [ -f /etc/systemd/system/humanode.service ]
+	then
+		systemctl stop humanode&& \
+		systemctl disable humanode
+		sleep 2
+fi
 
-# move humanode folder to /root
-cp humanode /root
+if [ -f /etc/systemd/system/ngrok.service ]
+	then
+		systemctl stop ngrok&& \
+		systemctl disable ngrok
+		sleep 2
+fi
 
-install -m 0755 "./binaries/$(uname -s)-$(uname -m)/humanode-peer" /usr/local/bin
-humanode-peer key insert --key-type aura --suri "$SECRET_PHRASE" --chain chainspec.json
+echo "Install packages ..."
+apt install curl jq wget unzip -y
+sleep 2
 
-# Make Humanode startup script
-printf "exec 1> >(logger -t humanode) 2>&1
-exec /usr/local/bin/humanode-peer --name "$NAME" --validator --chain chainspec.json --rpc-url "$RPC_URL" --rpc-cors all" > $HOME/humanode/start.sh
-chmod +x $HOME/humanode/start.sh
+if [ -f /usr/bin/ngrok ]
+	then
+		echo "Found ngrok in /usr/bin ..."
+	else
+		wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+		unzip ngrok-stable-linux-amd64.zip
+		rm ngrok-stable-linux-amd64.zip
+		mv ./ngrok /usr/bin/ngrok
+		chmod +x /usr/bin/ngrok
+		sleep 2
+fi
 
-# Create Humanode service
-printf "[Unit]
-Description=humanode
-After=network.service
+echo Copy new files from $USER_HOME/humanode to $HOME/humanode ...
+#move humanode folder to /root
+if [ -d $USER_HOME/humanode ]
+	then
+		echo Found $HOME/humanode ...
+		sleep 2
+		mkdir $HOME/humanode
+		cp $USER_HOME/humanode/chainspec.json $HOME/humanode/chainspec.json
+		cp $USER_HOME/humanode/binaries/$(uname -s)-$(uname -m)/humanode-peer $HOME/humanode/humanode-peer
+		sleep 2
+	else
+		echo TODO: unzip humanode-testnet1.zip $USER_HOME/humanode \
+			 then run that script again ...
+		exit
+fi
 
-[Service]
-Type=simple
-User=$USER
-WorkingDirectory=$HOME/humanode
-ExecStart=/bin/bash $HOME/humanode/start.sh
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/humanode.service
-
-# Enable & run Humanode service
-systemctl daemon-reload && \
-systemctl enable humanode && \
-systemctl start humanode && \
-systemctl restart systemd-journald
-
-# View last 50 lines of Humanode log
-journalctl -u humanode -n 50 | cat
-
-echo "Setting up Ngrok service..."
-
+echo "Configure & run ngrok.service..."
 # Make Ngrok config file
-rm -rf /root/.ngrok2
+rm -rf $HOME/.ngrok2
 mkdir $HOME/.ngrok2
 printf "authtoken: $NGROK_TOKEN
 tunnels:
@@ -71,11 +79,54 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/ngrok.service
 
+systemctl daemon-reload
 # Enable & run Ngrok service
-chmod +x /usr/bin/ngrok
-systemctl daemon-reload && \ 
-systemctl enable ngrok && \ 
-systemctl start ngrok
+systemctl enable ngrok && \
+systemctl start ngrok && \
+systemctl status ngrok
+# View Ngrok status
+# curl localhost:4040/api/tunnels | jq 
+curl localhost:4040/api/tunnels | jq > $HOME/humanode/ngrok_tunnels.json
+RPC_URL=$(echo "$public_url" | jq '.tunnels[0].public_url' $HOME/humanode/ngrok_tunnels.json)
+sleep 2
+
+cd $HOME/humanode
+install -m 0755 "humanode-peer" /usr/local/bin
+humanode-peer key insert --key-type aura --suri "$SECRET_PHRASE" --chain chainspec.json
+echo "Make Humanode startup script ..."
+printf "exec 1> >(logger -t humanode) 2>&1
+exec /usr/local/bin/humanode-peer --name "$NAME" --validator --chain chainspec.json --rpc-url "$RPC_URL" --rpc-cors all" > $HOME/humanode/start.sh
+chmod +x $HOME/humanode/start.sh
+ls
+sleep 2
+ 
+# View last 50 lines of Humanode log
+# journalctl -u humanode -n 50 | cat
+
+echo "Configure & run humanode.service..."
+# Create Humanode service
+printf "[Unit]
+Description=humanode
+After=network.service
+
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME/humanode
+ExecStart=/bin/bash $HOME/humanode/start.sh
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/humanode.service
+
+systemctl daemon-reload
+# Enable & run Humanode service
+systemctl enable humanode && \
+systemctl start humanode && \
+systemctl restart systemd-journal
+
+systemctl status humanode
+journalctl -u humanode -f
 
 # View Ngrok status
 curl localhost:4040/api/tunnels | jq

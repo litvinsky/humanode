1a80a7962e08a796bcac4d1dcc3ae8e0537ca3fa
surugh <35753081+surugh@users.noreply.github.com>
2021-10-18 13:31:55 +0300

    Update telegram-notify.sh

--- telegram-notify.sh
+++ telegram-notify.sh
@@ -29,7 +29,6 @@ rm -f /tmp/tg-export > /dev/null > /usr/bin/telegram-send
 chmod +x /usr/bin/telegram-send && \
 chown root:root /usr/bin/telegram-send
 #
-#
 # Add script to crontab (Run every 15 minutes)
 crontab -l | { cat; echo "*/15 * * * * /$HOME/humanode/notify.sh >/dev/null 2>&1"; } | crontab -
 #

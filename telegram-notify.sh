063506369bf59a080e293a1ae2472a6fa2a33371
surugh <35753081+surugh@users.noreply.github.com>
2021-10-18 13:30:57 +0300

    Update telegram-notify.sh

--- telegram-notify.sh
+++ telegram-notify.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-
+#
 #Make telegram-send script and move it to /usr/bin folder
 printf "#!/bin/bash
-GROUP_ID="TELEGRAM_GROUP_ID"
-BOT_TOKEN="YOUR_TELEGRAM_TOKEN"
-curl -s --data "text=$1" --data "chat_id=$GROUP_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' > /dev/null" > /usr/bin/telegram-send
+#
+# script for sending TEXT/FILE to Telegram
+#
+# Usage: telegram-send "<text>" or "</path/to/file>"
+# ..."folder non-exist" errors may be ignored
+#
+SEND_ME=$1
+CHAT_ID=""
+BOT_TOKEN=""
+#
+# Sending text-message
+curl --socks5-basic \
+-X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
+-d chat_id=$CHAT_ID -d text="$SEND_ME"
+#
+# sending file-message
+cat "$SEND_ME" > /tmp/tg-export && \
+curl --socks5-basic \
+-s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendDocument \
+-F chat_id=$CHAT_ID -F document=@/tmp/tg-export
+#
+# cleaning
+rm -f /tmp/tg-export > /dev/null > /usr/bin/telegram-send
+#
 chmod +x /usr/bin/telegram-send && \
 chown root:root /usr/bin/telegram-send
-
-#Add script to crontab (Run every 15 minutes)
+#
+#
+# Add script to crontab (Run every 15 minutes)
 crontab -l | { cat; echo "*/15 * * * * /$HOME/humanode/notify.sh >/dev/null 2>&1"; } | crontab -
-
-#Send test message to Telegram
-telegram-send "Test!"
+#
+# Send test message to Telegram
+telegram-send "Working ..."
